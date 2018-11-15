16780294f0f23c40f2ed4dd9a90e4cb9ed4d89ec
Meisuleen Kased <meisuleen@gmail.com>
2018-11-15 18:02:04 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,36 @@
+require 'pry'
+def sort_array_asc(num)
+  num.sort {|x,y| (x<=>y)}
+end
 
+def sort_array_desc(num)
+  num.sort.reverse
+end
+
+def sort_array_char_count(char)
+  char.sort {|a,b| a.length <=> b.length}
+end
+
+def swap_elements(ele)
+  ele.sort {|a,b| a[1] <=> b[2]}
+end
+
+def reverse_array(arr)
+  arr.reverse
+end
+
+def kesha_maker(arr)
+  arr.each {|a| a[2] = "$"}
+end
+
+def find_a(word)
+  word.select {|arr| arr.start_with?("a")}
+end
+
+def sum_array(array)
+  array.inject(0) {|sum,x| sum + x }
+end
+
+def add_s(arr)
+  arr.collect.each_with_index {|s| s << "s"}
+end
