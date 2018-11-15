8865246c3c4f7c23dd4f197e09d8b24326ceec6b
Meisuleen Kased <meisuleen@gmail.com>
2018-11-15 18:48:19 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -32,6 +32,6 @@ def sum_array(array)
 end
 
 def add_s(arr)
-    arr.each_with_index.collect{|word, i| word + "s" if i != 1}
+    arr.each_with_index.collect{|word, i| if i != 1 word + "s"}
   end
 end
